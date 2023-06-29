35c3b235dd086ceb0b062e19d46bbf31d8a4a430
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-06-29 18:14:19 +0700

    metode untuk element lebih kecil dari currentnode. info

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -52,7 +52,16 @@ public:
 	}
 
 	void Search(string element, Node*& parent, Node*& currentNode) {
-
+		// this function searches the currentNode of the specified Node as well as the current Node of ts parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element)) {
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
 	}
 };
 int main()
